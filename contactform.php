8d94ec65e3fe52c46d3211b3ca79ca60beb0eb13
Radoslav <Radoslaw.Begej@Gmail.com>
2018-10-03 15:17:34 +0200

    mailer

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php 
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "radoslaw.begej@gmail.com";
+    $headers = "From: ".$email;
+    $txt = "Masz woadomość od ".$name.".\n\n".$message;
+
+    mail($mailTo,$subject,$txt,$headers);
+    header("Location: index.php?mailsend");
+}
